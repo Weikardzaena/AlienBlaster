998f72ede4efca971cf21ee41bc6a1eedd0337c3
Weikardzaena <g.nikolai.kotula@gmail.com>
2018-03-10 15:31:50 -0700

    7: Ship now fires automatically when the key is held down

--- Alien-Blaster/Assets/Controllers/PlayerController.cs
+++ Alien-Blaster/Assets/Controllers/PlayerController.cs
@@ -1,22 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
     public float MaxSpeed = 10.0f;
+    public float AutoFirePeriod = 0.75f;
+
+    private float mNextFireTime = 0.0f;
 
 	// Use this for initialization
-	void Start () {
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        if (Input.GetButtonDown("Fire1")) {
-            Fire();
+    void Update ()
+    {
+        // Reset the fire time when the fire button is released:
+        if (Input.GetButtonUp("Fire1")) {
+            mNextFireTime = 0.0f;
         }
 
+        // If the fire button is down, check if it's time to fire again:
+        if (Input.GetButton("Fire1")) {
+            mNextFireTime -= Time.deltaTime;
+
+            // The fire period has expired, so fire and reset the timer:
+            if (mNextFireTime < 0.0f) {
+                Fire();
+                mNextFireTime = AutoFirePeriod;
+            }
+        }
+
+        // Move:
         float horizDelta = Input.GetAxis("Horizontal") * Time.deltaTime * MaxSpeed;
         float vertDelta = Input.GetAxis("Vertical") * Time.deltaTime * MaxSpeed;
 
