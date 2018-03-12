0ffd80b61e5acb16fce0963bfb14a131c2f0c3f7
Weikardzaena <g.nikolai.kotula@gmail.com>
2018-03-12 15:57:23 -0600

    10: Creates DOT damage type
    The Damage Over Time (DOT) component will deal damage to all game objects
    within its bounding box every Period number of seconds.  Both the period and
    the amount of damage can be tweaked.
    
    Note!  You must define a Trigger collision component in addition to your DOT
    component or else the component won't know what to damage.

---
+++ Alien-Blaster/Assets/Components/Weapons/Damage Types/DOTComp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DOTComp : MonoBehaviour
+{
+    public UInt32 Damage = 1;
+    public float DamagePeriod = 0.1f;
+
+    private UInt32 mDamage;
+    private float mDamagePeriod;
+    private float mNextDamageTime = 0.0f;
+    private List<DamageableComp> mDamageableList = new List<DamageableComp>();
+
+    // Use this for initialization
+    void Start()
+    {
+        mDamage = Damage;
+        mDamagePeriod = DamagePeriod;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        mNextDamageTime -= Time.deltaTime;
+
+		if (mNextDamageTime < 0.0f) {
+            mNextDamageTime = mDamagePeriod;
+
+            // Ensure that we cull out all objects that are no longer valid:
+            mDamageableList.RemoveAll(x => x == null);
+
+            foreach (DamageableComp comp in mDamageableList) {
+                comp.DealDamage(mDamage);
+            }
+        }
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        DamageableComp otherDamageable = other.GetComponent<DamageableComp>();
+
+        // We don't want duplicate entries in our list of things to damage:
+        if (otherDamageable != null) {
+            mDamageableList.Add(otherDamageable);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        DamageableComp otherDamageable = other.GetComponent<DamageableComp>();
+        // Always cull null before using something like List.RemoveAll():
+        mDamageableList.RemoveAll(x => x == null);
+
+        // Remove all instances of the damage component:
+        if ((otherDamageable != null)) {
+            mDamageableList.RemoveAll(x => x.GetInstanceID() == otherDamageable.GetInstanceID());
+        }
+    }
+}
