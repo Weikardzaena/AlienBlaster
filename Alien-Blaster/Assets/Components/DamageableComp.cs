9798d735540ce287bb2a4c5ab1134d319724154e
Weikardzaena <g.nikolai.kotula@gmail.com>
2018-03-12 13:57:33 -0600

    10: Creates basic point damage framework
    Four components are created:  Damageable, Destroyable, Health, and a Point
    Damage type.
    
    Damageable is reserved for things that can be hit by weapons/debris.
    
    Destroyable is reserved for things that can despawn (player ship, lasers,
    enemies, etc.).
    
    Health is reserved solely for tracking health; it does not know/care about the
    type of damage or what should happen to the object when its health reaches
    zero.
    
    Point Damage is used for single target damage.  A laser is a Point Damage since
    the laser hits the object and then despawns itself.

---
+++ Alien-Blaster/Assets/Components/DamageableComp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class DamageableComp : MonoBehaviour
+{
+    private HealthComp mHealthComp;
+    private DestroyableComp mDestroyableComp;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mHealthComp = GetComponent<HealthComp>();
+        mDestroyableComp = GetComponent<DestroyableComp>();
+	}
+
+    public void DealDamage(UInt32 damage)
+    {
+        Debug.Log(name + " DealDamage()");
+
+        // If we have a health component, subtrace the damage from the health:
+        if (mHealthComp != null) {
+            UInt32 newHealth = mHealthComp.SubtractHealth(damage);
+            
+            // If we're at 0 health and our GameObject is destroyable, destroy self:
+            if (newHealth == 0 && mDestroyableComp != null) {
+                mDestroyableComp.DestroySelf();
+            }
+        }
+    }
+}
---
+++ Alien-Blaster/Assets/Components/Weapons/PointDamageComp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class PointDamageComp : MonoBehaviour
+{
+    public UInt32 DamageVal = 1;
+
+    private UInt32 mDamage;
+    private DestroyableComp mDestroyableComp;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mDamage = DamageVal;
+        mDestroyableComp = GetComponent<DestroyableComp>();
+	}
+
+    public void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(name + " OnCollisionEnter()");
+        // There's no way of getting around checking for the Damageable Component at hit
+        // since we don't know what object we're going to hit ahead of time.  And since
+        // we're not going to be calling this every frame, there's no performance issue
+        // (not tested, but it makes sense to think that way).
+        var damageComp = col.gameObject.GetComponent<DamageableComp>();
+
+        if (damageComp != null) {
+            damageComp.DealDamage(mDamage);
+
+            // If the hit object is damageable, destroy ourself since we want to despawn the game object.
+            if (mDestroyableComp != null) {
+                mDestroyableComp.DestroySelf();
+            }
+        }
+    }
+}
