f3aadf7668a5aa721889a53ab8f7d5f38aecaf39
Weikardzaena <g.nikolai.kotula@gmail.com>
2018-03-12 22:38:47 -0600

    Creates a timed life component

---
+++ Alien-Blaster/Assets/Components/General/TimedLifeComp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedLifeComp : MonoBehaviour
+{
+    public float Lifetime = 1.0f;
+
+    private float mTimeRemaining;
+    private DestroyableComp mDestroyableComp;
+
+	// Use this for initialization
+	void Start () {
+        mDestroyableComp = gameObject.GetComponent<DestroyableComp>();
+        mTimeRemaining = Lifetime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mTimeRemaining -= Time.deltaTime;
+
+        if (mTimeRemaining < 0.0f) {
+            if (mDestroyableComp) {
+                mDestroyableComp.DestroySelf();
+            } else {
+                Destroy(gameObject);
+            }
+        }
+	}
+}
