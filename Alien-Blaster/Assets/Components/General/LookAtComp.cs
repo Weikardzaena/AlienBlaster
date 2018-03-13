7c1623c70bf05ba65e2f4adcce3b2a399153a6b1
Weikardzaena <g.nikolai.kotula@gmail.com>
2018-03-13 17:31:25 -0600

    15: Creates basic missile prefab
    Adds a "Look At" component whose job is soley to turn directly at a given target.

---
+++ Alien-Blaster/Assets/Components/General/LookAtComp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtComp : MonoBehaviour
+{
+    public float TurnSpeed;
+    public GameObject mTarget;
+
+    private float mTurnSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        mTurnSpeed = TurnSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(mTarget.transform);
+    }	
+
+    public void SetTarget(GameObject newTarget)
+    {
+        mTarget = newTarget;
+    }
+}
