d391fbffd4e294a438db1eae76bcbf0791c83f48
Weikardzaena <g.nikolai.kotula@gmail.com>
2018-03-12 21:51:34 -0600

    10: Refactors DOT framework
    With previous implementation the DOT source dealt damage and handled targets.
    This commit breaks the logic apart to make it eaiser to apply damage on the fly
    with little performance hit.

---
+++ Alien-Blaster/Assets/Components/Weapons/AOE_DOT_Source.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AOE_DOT_Source : MonoBehaviour
+{
+    public UInt32 Damage = 1;
+    public float DamagePeriod = 0.1f;
+
+    private UInt32 mDamage;
+    private float mPeriod;
+    private HashSet<DOTSrcMgrComp> mTargets = new HashSet<DOTSrcMgrComp>();
+
+	// Use this for initialization
+	void Start () {
+        ChangeDmgAndPeriod(Damage, DamagePeriod);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        DOTSrcMgrComp otherDOTmgr = other.gameObject.GetComponent<DOTSrcMgrComp>();
+        if (otherDOTmgr) {
+            otherDOTmgr.AddDOTSource(mPeriod, mDamage, this);
+            mTargets.Add(otherDOTmgr);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        DOTSrcMgrComp otherDOTmgr = other.gameObject.GetComponent<DOTSrcMgrComp>();
+        if (otherDOTmgr) {
+            otherDOTmgr.RemoveDOTSource(this);
+            mTargets.Remove(otherDOTmgr);
+        }
+    }
+
+    public void ChangeDamage(UInt32 newDamage)
+    {
+        mDamage = newDamage;
+
+        UpdateAllTargets();
+    }
+
+    public void ChangePeriod(float newPeriod)
+    {
+        mPeriod = newPeriod;
+
+        UpdateAllTargets();
+    }
+
+    public void ChangeDmgAndPeriod(UInt32 newDamage, float newPeriod)
+    {
+        mDamage = newDamage;
+        mPeriod = newPeriod;
+
+        UpdateAllTargets();
+    }
+
+    private void UpdateAllTargets()
+    {
+        foreach (var dotComp in mTargets) {
+            dotComp.AddDOTSource(mPeriod, mDamage, this);
+        }
+    }
+}
---
+++ Alien-Blaster/Assets/Components/Weapons/Damage Types/DOTApplier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class DOTApplier
+{
+    public float Period { get; private set; }
+    public UInt32 Damage { get; private set; }
+
+    public DamageableComp Target { get; private set; }
+
+    private float mNextFireTime = 0.0f;
+
+    public DOTApplier(float period, UInt32 damage, DamageableComp target)
+    {
+        Period = period;
+        Damage = damage;
+        Target = target;
+    }
+
+    public void Update(float deltaTime)
+    {
+        mNextFireTime -= deltaTime;
+
+        if ((mNextFireTime < 0.0f) && Target) {
+            mNextFireTime = Period;
+            Target.DealDamage(Damage);
+        }
+    }
+}
