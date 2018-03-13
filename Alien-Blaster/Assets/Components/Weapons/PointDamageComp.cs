d1b18a003d0b74659e6725cd109342a902bafe9a
Weikardzaena <g.nikolai.kotula@gmail.com>
2018-03-12 22:49:28 -0600

    10: Refactor Point Damage
    The point damage was coupled to the Destroyable Component, which was bad.  The
    refactor now only does a single instance of damage to whatever the parent hits.
    The "Destroy self when it hits something" will be another component.

--- Alien-Blaster/Assets/Components/Weapons/PointDamageComp.cs
+++ Alien-Blaster/Assets/Components/Weapons/PointDamageComp.cs
@@ -6,31 +6,24 @@ public class PointDamageComp : MonoBehaviour
     public UInt32 DamageVal = 1;
 
     private UInt32 mDamage;
-    private DestroyableComp mDestroyableComp;
 
 	// Use this for initialization
 	void Start ()
     {
         mDamage = DamageVal;
-        mDestroyableComp = GetComponent<DestroyableComp>();
 	}
 
-    public void OnCollisionEnter(Collision col)
+    public void OnTriggerEnter(Collider other)
     {
-        Debug.Log(name + " OnCollisionEnter()");
+        Debug.Log(name + " PointDamageComp::OnTriggerEnter()");
         // There's no way of getting around checking for the Damageable Component at hit
         // since we don't know what object we're going to hit ahead of time.  And since
         // we're not going to be calling this every frame, there's no performance issue
         // (not tested, but it makes sense to think that way).
-        var damageComp = col.gameObject.GetComponent<DamageableComp>();
+        var damageComp = other.gameObject.GetComponent<DamageableComp>();
 
-        if (damageComp != null) {
+        if (damageComp) {
             damageComp.DealDamage(mDamage);
-
-            // If the hit object is damageable, destroy ourself since we want to despawn the game object.
-            if (mDestroyableComp != null) {
-                mDestroyableComp.DestroySelf();
-            }
         }
     }
 }
