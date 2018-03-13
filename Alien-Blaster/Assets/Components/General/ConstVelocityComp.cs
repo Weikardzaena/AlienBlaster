20509ebdb2f4005bef60f7403c9df4bf1f1b64e8
Weikardzaena <g.nikolai.kotula@gmail.com>
2018-03-13 15:57:11 -0600

    10: Add constant velocity component to laser prefab
    Now the laser prefab has a component where it moves along its +Y axis.  AKA,
    the laser moves forward.

---
+++ Alien-Blaster/Assets/Components/General/ConstVelocityComp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstVelocityComp : MonoBehaviour
+{
+    public float Speed = 1.0f;
+    public Vector3 Direction = new Vector3(0, 0, 0);
+
+    private float mSpeed;
+    private Vector3 mDirection;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mDirection = Direction.normalized;
+        mSpeed = Speed;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Translate(mDirection * mSpeed * Time.deltaTime);
+	}
+}
