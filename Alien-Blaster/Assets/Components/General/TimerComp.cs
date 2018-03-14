8f519a408e0cca9ba78eae2797985c5f43a844d0
Weikardzaena <g.nikolai.kotula@gmail.com>
2018-03-13 21:08:58 -0600

    17: Creates prefab for stationary bomb
    Refactors the Timed Life Component to instead be just a generic timer with a
    bindable event.  This way, the bomb's Explosion component's Detonate() method
    can be called when the timer runs out.  Additionally, the timer also calls the
    Destroyable component's methods to clean up the object.

---
+++ Alien-Blaster/Assets/Components/General/TimerComp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimerComp : MonoBehaviour
+{
+    [Serializable]
+    public class TimerExpired : UnityEvent
+    { }
+
+    public float Duration = 1.0f;
+    public TimerExpired OnTimerExpired;
+
+    public bool IsFinished { get; private set; }
+
+    private float mTimeRemaining;
+
+	// Use this for initialization
+	void Start () {
+        mTimeRemaining = Duration;
+        IsFinished = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!IsFinished) {
+            mTimeRemaining -= Time.deltaTime;
+
+            if (mTimeRemaining < 0.0f) {
+                OnTimerExpired.Invoke();
+                IsFinished = true;
+            }
+        }
+	}
+}
