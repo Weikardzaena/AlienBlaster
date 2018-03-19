dba0428f080363f65f40e81fffbe4c34964920fb
Weikardzaena <g.nikolai.kotula@gmail.com>
2018-03-19 10:20:20 -0600

    24: Adds object pool data component
    An object pool holds a number of Game Objects that can be fetched to avoid
    having to destroy and reinstantiate reusable objects over and over again.  E.G.
    lasers.  Instead of destroying and spawning them, just get a laser prefab from
    the laser object pool, use it, then deactivate it so the next client that wants
    a laser can have it.

---
+++ Alien-Blaster/Assets/Components/Data/DObjectPool.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DObjectPool : MonoBehaviour
+{
+    [Tooltip("The prefab of the object that this pool will contain.")]
+    public GameObject ObjectPrefab;
+
+    [Tooltip("How many objects this pool will contain at start.")]
+    public UInt16 InitialQuantity = 10;
+
+    [Tooltip("Should this pool create new instances of the prefab if none are available?")]
+    public bool ShouldExpand = true;
+
+    private List<GameObject> mObjectPool;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (UInt16 i = 0; i < InitialQuantity; i++) {
+            mObjectPool.Add(Instantiate(ObjectPrefab));
+        }
+    }
+
+    public GameObject GetObject()
+    {
+        for (UInt16 i = 0; i < mObjectPool.Count; i++) {
+            if (!mObjectPool[i].activeInHierarchy) {
+                return mObjectPool[i];
+            }
+        }
+
+        if (ShouldExpand) {
+            GameObject newObj = Instantiate(ObjectPrefab);
+            return newObj;
+        }
+
+        return null;
+    }
+}
