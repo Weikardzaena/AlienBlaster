03f917cd6c6fa9115736b64b215ad85a37178539
Weikardzaena <g.nikolai.kotula@gmail.com>
2018-03-19 17:01:12 -0600

    Remove unnecessary log statement in BCollisionDamage.cs

--- Alien-Blaster/Assets/Components/Behaviors/Damage/BCollisionDamage.cs
+++ Alien-Blaster/Assets/Components/Behaviors/Damage/BCollisionDamage.cs
@@ -15,7 +15,6 @@ public class BCollisionDamage : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
-        Debug.Log(name + " PointDamageComp::OnTriggerEnter()");
         // There's no way of getting around checking for the Damageable Component at hit
         // since we don't know what object we're going to hit ahead of time.  And since
         // we're not going to be calling this every frame, there's no performance issue
