b609795768b89f072f5203a0a31a9fa3d96cc5de
Weikardzaena <g.nikolai.kotula@gmail.com>
2018-03-19 11:15:47 -0600

    21: Creates Recyclable component
    The recyclable component will reset all of the resettable components and
    deactivate itself.
    
    Also updates attack prefabs to use Recyclable instead of Destroyable.
    
    NOTE:  When dealing with timers, be sure to always call Recycle last, or else
    the object will deactivate before finishing its other timer events.

---
+++ Alien-Blaster/Assets/Components/Attributes/ARecyclable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ARecyclable : MonoBehaviour
+{
+    private IResettable[] mComponents;
+
+    // Use this for initialization
+    void Start()
+    {
+        mComponents = GetComponents<IResettable>();
+    }
+
+    public void Recycle()
+    {
+        foreach (IResettable comp in mComponents) {
+            comp.Reset();
+        }
+
+        gameObject.SetActive(false);
+    }
+}
---
+++ Alien-Blaster/Assets/Components/Behaviors/Object Lifecycle/Recycle/BRecycleOnTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BRecycleOnTrigger : MonoBehaviour
+{
+    private ARecyclable mRecycleComp;
+
+    void Start()
+    {
+        mRecycleComp = GetComponent<ARecyclable>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (mRecycleComp) {
+            mRecycleComp.Recycle();
+        }
+    }
+}
