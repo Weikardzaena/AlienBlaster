9fb9a124da15aa3b172740409b8e2f3e51929715
Weikardzaena <g.nikolai.kotula@gmail.com>
2018-03-20 12:13:11 -0600

    Adds repeat timer
    Executes the same as a single fire timer, but continuously fires and resets
    itself to the duration.

---
+++ Alien-Blaster/Assets/Components/Behaviors/Utility/BRepeatTimer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BRepeatTimer : MonoBehaviour, IResettable
+{
+    [Serializable]
+    public class TimerFired : UnityEvent
+    { }
+
+    public float Duration = 1.0f;
+    public TimerFired OnTimerFired;
+
+    private float mTimeRemaining = 0.0f;
+
+    // Use this for initialization
+    void OnEnable()
+    {
+        Reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mTimeRemaining -= Time.deltaTime;
+        if (mTimeRemaining < 0.0f) {
+            Debug.Log(name + " timer fire!");
+            OnTimerFired.Invoke();
+            Reset();
+        }
+    }
+
+    public void Reset()
+    {
+        mTimeRemaining = Duration;
+    }
+}
