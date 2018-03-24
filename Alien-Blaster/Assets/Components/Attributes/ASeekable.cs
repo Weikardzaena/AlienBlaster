bb43bda69a49cf949da217a30a51dbd1b51bb758
Weikardzaena <g.nikolai.kotula@gmail.com>
2018-03-23 21:14:26 -0600

    33: Adds elapsed lifetime interface
    If something depends on how long an object has been alive for, they can add a
    component to it which will keep track of how long it has been enabled.  Since
    we're dealing with scriptable behaviors, this also introduces a Seekable
    interface.  If you want to rewind or fast-forward an object's behavior, you can
    do that.

---
+++ Alien-Blaster/Assets/Components/Attributes/ASeekable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ASeekable : MonoBehaviour
+{
+    public DObjectElapsedTime ObjectElapsedTime;
+
+    private void OnEnable()
+    {
+        if (!ObjectElapsedTime)
+            ObjectElapsedTime = GetComponent<DObjectElapsedTime>();
+    }
+
+    public void StartRewind(float seconds, float duration)
+    {
+        if (ObjectElapsedTime)
+            StartCoroutine(ObjectElapsedTime.Rewind(seconds, duration));
+    }
+
+    public void StartFastForward(float seconds, float duration)
+    {
+        if (ObjectElapsedTime)
+            StartCoroutine(ObjectElapsedTime.FastForward(seconds, duration));
+    }
+}
---
+++ Alien-Blaster/Assets/Components/Data/DObjectElapsedTime.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DObjectElapsedTime : MonoBehaviour, IResettable
+{
+    public float ElapsedTime { get; private set; }
+
+    private bool mIsSeeking;
+
+    private void OnEnable()
+    {
+        Reset();
+    }
+
+    private void Update()
+    {
+        if (!mIsSeeking)
+            ElapsedTime += Time.deltaTime;
+    }
+
+    public IEnumerator Rewind(float seconds, float duration)
+    {
+        if (!mIsSeeking) {
+            mIsSeeking = true;
+
+            for (float i = 0.0f; i < duration; i += Time.deltaTime) {
+                ElapsedTime -= Time.deltaTime / duration * seconds;
+                yield return null;
+            }
+
+            mIsSeeking = false;
+        }
+    }
+
+    public IEnumerator FastForward(float seconds, float duration)
+    {
+        if (!mIsSeeking) {
+            mIsSeeking = true;
+
+            for (float i = 0.0f; i < duration; i += Time.deltaTime) {
+                ElapsedTime += Time.deltaTime / duration * seconds;
+                yield return null;
+            }
+
+            mIsSeeking = false;
+        }
+    }
+
+    public void Reset()
+    {
+        ElapsedTime = 0.0f;
+        mIsSeeking = false;
+    }
+}
